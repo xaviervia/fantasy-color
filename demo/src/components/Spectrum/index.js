3ccb81dff2bb163e98c3d5b48f977c37bd8aa141
Xavier Via <fernando.via@gmail.com>
2018-07-18 11:46:30 +0200

    More work on the spectrum

--- demo/src/components/Spectrum/index.js
+++ demo/src/components/Spectrum/index.js
@@ -8,7 +8,8 @@ import {
   MeshBasicMaterial,
   Mesh,
 } from 'three'
-import Color from '../../../'
+import boundaries from './boundaries'
+import Color from '../../../../'
 
 const cube = (geometry, color) => {
   const material = new MeshBasicMaterial({
@@ -19,64 +20,6 @@ const cube = (geometry, color) => {
   cube.position.y = color[1]
   cube.position.z = color[2]
   return cube
-}
-
-const boundaries = () => {
-  const redZ = new Mesh(
-    new BoxGeometry(1, 1, 256),
-    new MeshBasicMaterial({
-      color: 0xff0000,
-    })
-  )
-  redZ.position.x = 256
-  redZ.position.z = 128
-
-  const redY = new Mesh(
-    new BoxGeometry(1, 256, 1),
-    new MeshBasicMaterial({
-      color: 0xff0000,
-    })
-  )
-  redY.position.x = 256
-  redY.position.y = 128
-
-  const greenX = new Mesh(
-    new BoxGeometry(256, 1, 1),
-    new MeshBasicMaterial({
-      color: 0x00ff00,
-    })
-  )
-  greenX.position.y = 256
-  greenX.position.x = 128
-
-  const greenZ = new Mesh(
-    new BoxGeometry(1, 1, 256),
-    new MeshBasicMaterial({
-      color: 0x00ff00,
-    })
-  )
-  greenZ.position.y = 256
-  greenZ.position.z = 128
-
-  const blueX = new Mesh(
-    new BoxGeometry(256, 1, 1),
-    new MeshBasicMaterial({
-      color: 0x0000ff,
-    })
-  )
-  blueX.position.z = 256
-  blueX.position.x = 128
-
-  const blueY = new Mesh(
-    new BoxGeometry(1, 256, 1),
-    new MeshBasicMaterial({
-      color: 0x0000ff,
-    })
-  )
-  blueY.position.z = 256
-  blueY.position.y = 128
-
-  return [redY, redZ, greenX, greenZ, blueX, blueY]
 }
 
 class Spectrum extends Component {
@@ -122,7 +65,7 @@ class Spectrum extends Component {
 
     this.scene.add(axesHelper)
 
-    boundaries().forEach(boundary => {
+    boundaries.forEach(boundary => {
       this.scene.add(boundary)
     })
 
@@ -140,7 +83,7 @@ class Spectrum extends Component {
 
     this.scene.add(axesHelper)
 
-    boundaries().forEach(boundary => {
+    boundaries.forEach(boundary => {
       this.scene.add(boundary)
     })
 
