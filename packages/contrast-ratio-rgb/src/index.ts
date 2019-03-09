d239a8cfb27d60987ade53fc622be9422e4eb99f
Xavier Via <fernando.via@gmail.com>
2019-03-09 22:23:34 +0100

    🚧

--- packages/contrast-ratio-rgb/src/index.ts
+++ packages/contrast-ratio-rgb/src/index.ts
@@ -1,11 +1,9 @@
 import { RGB } from '@fantasy-color/types/src'
 import luminanceRgb from '@fantasy-color/luminance-rgb/src'
+import contrastRatioLuminance from '@fantasy-color/contrast-ratio-luminance/src'
 
-export default (color1: RGB, color2: RGB): number => {
-  const luminance1 = luminanceRgb(color1)
-  const luminance2 = luminanceRgb(color2)
-
-  return luminance1 >= luminance2
-    ? (luminance1 + .05) / (luminance2 + .05)
-    : (luminance2 + .05) / (luminance1 + .05)
-}
+export default (color1: RGB, color2: RGB): number =>
+  contrastRatioLuminance(
+    luminanceRgb(color1),
+    luminanceRgb(color2)
+  )
